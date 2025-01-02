401244ac0d44c65227bbe99d05424b667d5dc92d
William Chu <willchu207@gmail.com>
2025-01-01 23:13:23 -0800

    New plan: one model per store

--- src/model.py
+++ src/model.py
@@ -2,6 +2,7 @@ from preprocessor import Preprocessor
 from multi_stl import MultiSTL
 from varmax_wrapper import VARMAXWrapper
 
+# Contains a collection of models, one per store
 class ForecastingModel:
     def __init__(self):
         self.preprocessor = Preprocessor()
@@ -26,12 +27,15 @@ class ForecastingModel:
         self.target_label = target_label
         self.exog_label = exog_label
 
-        #self.fit_varmax()
+        self.fit_varmax()
 
     def fit_varmax(self):
         # Eliminate series with exactly zero sales on all days
         self.no_data = (self.data["sales"] == 0).all()
-        self.varmax.fit(self.data[self.target_label], self.data[self.exog_label])
+        self.varmax.fit(
+            self.data[self.target_label].loc[:, ~self.no_data],
+            self.data[self.exog_label].loc[:, ~self.no_data]
+        )
 
     def forecast(self, n):
         self.varmax.forecast(n)
---
+++ src/store_model.py
@@ -0,0 +1,10 @@
+class StoreModel:
+    def __init__(self):
+        self.stl = None
+        pass
+
+    def fit(self, endog, exog, p, q):
+        pass
+
+    def forecast(self, n):
+        pass
